e4170ce4319c12c2e304fbf7d796ec5c3a206efd
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-15 14:16:13 +0200

    Update hackaton_groep3.py

--- hackaton_groep3.py
+++ hackaton_groep3.py
@@ -12,7 +12,7 @@ with st.sidebar:
 
 # INTRO pagina
 if selected == 'Intro':
-    st.title("Hackaton - Groep 3 ")
+    st.title("Hackaton - Groep  ")
     
 
 # --------------------------------------------------------------------------
