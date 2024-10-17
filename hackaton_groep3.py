afc7c0830b36b5fd3cf9ebcb03ea1774a7f13cd4
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-17 18:55:04 +0200

    Update hackaton_groep3.py

--- hackaton_groep3.py
+++ hackaton_groep3.py
@@ -60,24 +60,24 @@ if selected == "Energievraag Sectoren":
 
 
   # HEATMAP
-  df1 = pd.read_excel("Data_verbruik_v8.xlsx")
+  with st.container():
+    st.subheader("Heatmap van Energieverbruik per Pand in Sloterdijk Poort Noord Amsterdam")
 
-  st.subheader("Heatmap van Energieverbruik per Pand in Sloterdijk Poort Noord Amsterdam")
+    df1 = pd.read_excel("Data_verbruik_v8.xlsx")
+    df1 = df1.dropna(subset=['Latitude', 'Longitude'])
 
-  df1 = df1.dropna(subset=['Latitude', 'Longitude'])
+    df1["Totaal verbruik per week (kWh)"] = df1[["Verbruik maandag", "Verbruik dinsdag", "Verbruik woensdag", 
+                                               "Verbruik donderdag", "Verbruik vrijdag"]].sum(axis=1)
 
-  df1["Totaal verbruik per week (kWh)"] = df1[["Verbruik maandag", "Verbruik dinsdag", "Verbruik woensdag", 
-                                           "Verbruik donderdag", "Verbruik vrijdag"]].sum(axis=1)
+    df1_grouped = df1.groupby(["pand", "Latitude", "Longitude"], as_index=False)["Totaal verbruik per week (kWh)"].sum()
 
-  df1_grouped = df1.groupby(["pand", "Latitude", "Longitude"], as_index=False)["Totaal verbruik per week (kWh)"].sum()
+    map_center = [52.395762704268726, 4.789355012543267]
+    m = folium.Map(location=map_center, zoom_start=15)
 
-  map_center = [52.395762704268726, 4.789355012543267]  # Coördinaten van het midden van het terrein
-  m = folium.Map(location=map_center, zoom_start=15)
+    heat_data = [[row['Latitude'], row['Longitude'], row['Totaal verbruik per week (kWh)']] for index, row in df1_grouped.iterrows()]
+    HeatMap(heat_data, radius=24, max_zoom=13).add_to(m)
 
-  heat_data = [[row['Latitude'], row['Longitude'], row['Totaal verbruik per week (kWh)']] for index, row in df1_grouped.iterrows()]
-  HeatMap(heat_data, radius=24, max_zoom=13).add_to(m)
-
-  st_folium(m, width=700, height=500)
+    st_folium(m, width=700, height=500)
 
 
   # DAGELIJKS, WEEKELIJKS, MAANDELIJKS PLOTS
