4c5aba4ae8096a8e186a749dbdf2f94df43c5759
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-17 21:19:36 +0200

    Update hackaton_groep3.py

--- hackaton_groep3.py
+++ hackaton_groep3.py
@@ -157,6 +157,9 @@ if selected == "Energievraag Sectoren":
 # ZONNEPANELEN pagina
 if selected == 'Zonnepanelen':
   st.title("Zonnepanelen")
+  
   st.subheader("Bestaande infrastructuur op het terrein")
-
   st.image("middenspanningskabels_ams.png", caption="Middenspanningskabels in Sloterdijk Poort Noord", use_column_width=True)
+  
+  st.subheader("Pandnummers Sloterijk Poort Noord")
+  st.image("panden_ams.png", caption="Panden in Sloterdijk Poort Noord", use_column_width=True)
