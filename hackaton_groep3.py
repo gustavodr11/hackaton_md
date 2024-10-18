7e5a89cc8ff67fe6a7a5974a1efee64ab4cf86ed
Gustavo de Rouville <129081063+gustavodr11@users.noreply.github.com>
2024-10-18 08:16:02 +0200

    Update hackaton_groep3.py

--- hackaton_groep3.py
+++ hackaton_groep3.py
@@ -177,8 +177,6 @@ if selected == "Energievraag Sectoren":
   # Toon de grafiek in Streamlit
   st.plotly_chart(fig_rotterdam)
 
-  st.write(df5.columns)
-
 
 
 
@@ -187,7 +185,7 @@ if selected == "Energievraag Sectoren":
     st.subheader("Heatmap van Energieverbruik per Pand in Dutch Fresh Port: Nieuw-Reijerwaard")
 
     df5 = pd.read_excel("rotterdam_verbruik.xlsx")
-
+    st.write(df5.columns)
     df5["Totaal verbruik per week (kWh)"] = df5[["Verbruik maandag", "Verbruik dinsdag", "Verbruik woensdag", 
                                                "Verbruik donderdag", "Verbruik vrijdag"]].sum(axis=1)
 
